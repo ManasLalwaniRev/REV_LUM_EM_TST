f8dcdd95c1e478ff4b6390d0869116cd575234b8
ManasLalwaniRev <Manas.Lalwani@revolvespl.com>
2025-10-23 15:11:17 -0400

    Fixes

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -183,7 +183,7 @@
 
 
 // STABLE 1 ends //
-
+console.log("VITE_API_BASE_URL VALUE:", import.meta.env.VITE_API_BASE_URL);
 import React, { useState } from 'react';
 
 const API_BASE_URL = 'http://localhost:5000/api';
