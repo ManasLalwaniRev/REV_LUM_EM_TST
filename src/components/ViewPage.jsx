d4301a93cf0c9053ffd5cb2b23d311ed3f96db91
ManasLalwaniRev <Manas.Lalwani@revolvespl.com>
2025-10-29 22:07:37 -0400

    Updates

--- src/components/ViewPage.jsx
+++ src/components/ViewPage.jsx
@@ -1130,7 +1130,7 @@ const formatDateForDisplay = (isoString) => {
       // Call the backend to generate the Excel file
       // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
       const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/entries`;
-      const response = await fetch(`${API_BASE_URL}/generate-excel`, {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/generate-excel`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ data: dataForSheet }),
