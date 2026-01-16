56e94630273dccb5ee1ec363125b15ebf8800f8e
ManasLalwaniRev <172045090+ManasLalwaniRev@users.noreply.github.com>
2026-01-15 20:10:16 -0500

    Testing New changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -289,7 +289,7 @@ export default function Sidebar({ currentPage, setCurrentPage, currentUserRole, 
   { page: 'accountant', label: 'SLA', icon: <Briefcase className="h-5 w-5" />, roles: ['admin', 'accountant'] },
     { page: 'bill', label: 'Billing', icon: <Book className="h-5 w-5" />, roles: ['user', 'admin', 'accountant'] },
   { page: 'user-profile', label: 'Settings & Profile', icon: <Settings className="h-5 w-5" />, roles: ['user', 'admin', 'accountant'] },
-  { page: 'bill', label: 'Billing', icon: <Book className="h-5 w-5" />, roles: ['user', 'admin', 'accountant'] },
+  // { page: 'bill', label: 'Billing', icon: <Book className="h-5 w-5" />, roles: ['user', 'admin', 'accountant'] },
   { page: 'about', label: 'About', icon: <Info className="h-5 w-5" />, roles: ['user', 'admin', 'accountant'] },
 
 ];
